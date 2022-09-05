b65cfbded3b1c4032ff1b3845a3e39f36b27d6bc
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-09-05 17:03:37 +0300

    SortArray descending

---
+++ dz_z54/Program.cs
@@ -0,0 +1,70 @@
+﻿int ReadInputData(string message)
+{
+    Console.WriteLine(message);
+    if (!int.TryParse(Console.ReadLine(), out var result))
+        Console.WriteLine("Все плохо");
+    return result;
+}
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++)
+    {
+        for (var j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.Next(1, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int numberOfRows = ReadInputData("Введите число строк (m)");
+int numberOfColumns = ReadInputData("Введите число столбцов (n)");
+
+if (numberOfRows <= 0 || numberOfColumns <= 0)
+{
+    Console.Write("Невозможно создать массив");
+    return;
+}
+
+var array = CreateArrayWithRandomNumbers(numberOfRows, numberOfColumns);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+SortArray(array);
+PrintArray(array);
